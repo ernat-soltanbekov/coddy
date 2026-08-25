22133492afb9124a407ac61afe399effbf088941
Yernat "GhostOfAstana" Soltanbekov <theastanadragon@gmail.com>
2026-08-25 10:38:07 +0500

    Implement retry logic using goto statement

---
+++ August-25-2026-TheGotoStatement/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func main() {
+    // Чтение входных данных
+    var maxRetriesStr string
+    var successAttemptStr string
+    fmt.Scanln(&maxRetriesStr)
+    fmt.Scanln(&successAttemptStr)
+    
+    // Преобразование входных данных в целые числа
+    maxRetries, _ := strconv.Atoi(maxRetriesStr)
+    successAttempt, _ := strconv.Atoi(successAttemptStr)
+    
+    // Инициализация счетчика попыток
+    attempt := 1
+    
+    // TODO: Напишите свой код ниже
+    // Используйте оператор goto с меткой для реализации логики повторных попыток
+    start:
+        if attempt == successAttempt {
+        fmt.Printf("Attempt %d succeeded\n", attempt)
+        return
+        }
+        fmt.Printf("Attempt %d failed\n", attempt)
+        attempt++
+        if attempt > maxRetries {
+            fmt.Printf("All attempts failed")
+            return
+        }
+        goto start
+}
