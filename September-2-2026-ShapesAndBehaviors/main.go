cc954375f54f71eb4f28097a74ded6a12daf1735
Yernat "GhostOfAstana" Soltanbekov <theastanadragon@gmail.com>
2026-09-02 14:25:36 +0500

    Implement Speaker interface with Person and Parrot

---
+++ September-2-2026-ShapesAndBehaviors/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+
+
+// TODO: Напишите свой код ниже
+// 1. Определите интерфейс Speaker
+type Speaker interface {
+    Speak() string
+}
+// 2. Определите структуры Person и Parrot
+type Person struct {
+    Name string
+}
+type Parrot struct {
+    Name string
+}
+// 3. Реализуйте методы Speak() для обеих структур
+func (a Person) Speak() string {
+    return fmt.Sprintf("Hello, I'm %s", a.Name)
+}
+func (b Parrot) Speak() string {
+    return fmt.Sprintf("Squawk! %s says hello!", b.Name)
+}
+// 4. Создайте функцию makeAllSpeak
+func makeAllSpeak(speakers []Speaker) {
+    for i := 0; i < len(speakers); i++ {
+        res := speakers[i].Speak()
+        fmt.Println(res)
+    }
+}
+// 5. Создайте объекты speaker на основе входных данных и сохраните их в срез
+// 6. Вызовите makeAllSpeak с вашим срезом
+func main() {
+	// Чтение входных данных
+	var numSpeakersStr string
+	var speakerTypesStr string
+	var speakerNamesStr string
+	
+	fmt.Scanln(&numSpeakersStr)
+	fmt.Scanln(&speakerTypesStr)
+	fmt.Scanln(&speakerNamesStr)
+	
+	// Парсинг входных данных
+	numSpeakers, _ := strconv.Atoi(numSpeakersStr)
+	speakerTypes := strings.Split(speakerTypesStr, ",")
+	speakerNames := strings.Split(speakerNamesStr, ",")
+    speakers := []Speaker{}
+    for i := 0; i < numSpeakers; i++ {
+        switch speakerTypes[i] {
+            case "person":
+            speakers = append(speakers, Person{Name: speakerNames[i]})
+            case "parrot":
+            speakers = append(speakers, Parrot{Name: speakerNames[i]})
+        }
+    }
+    makeAllSpeak(speakers)
+}
