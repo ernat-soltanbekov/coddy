cea98f50617d46758518227635d123f530f0d5a2
Yernat "GhostOfAstana" Soltanbekov <theastanadragon@gmail.com>
2026-09-15 12:26:51 +0500

    Initialize student data handling in main.go
    Implement basic structure and input handling for student data.

---
+++ September-15-2026-MapOfStructsSecond/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	// Чтение входных данных
+	var numStudentsStr string
+	var studentData string
+	fmt.Scanln(&numStudentsStr)
+	fmt.Scanln(&studentData)
+	studentData, err = strconv.Atoi(numStudentsStr)
+	if err != nil {
+		fmt.Printf("Invalid number: %v", err)
+		return
+	}
+	// TODO: Определите структуру Student здесь
+	type Student struct {
+		ID int
+		Grade string
+	}
+	// TODO: Создайте карту (map) для хранения студентов (имя как ключ, структура Student как значение)
+	
+	// TODO: Разберите данные о студентах и заполните карту
+	
+	// TODO: Выведите всех студентов в алфавитном порядке по имени
+	
+	// TODO: Рассчитайте и выведите статистику оценок
+	
+	// TODO: Найдите и выведите студента с самым высоким ID
+	
+	// TODO: Выведите общее количество студентов
+}
