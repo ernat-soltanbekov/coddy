7ef9238ae3263e1cf67f61555e38daf505753d34
Yernat "GhostOfAstana" Soltanbekov <theastanadragon@gmail.com>
2026-09-16 09:38:32 +0500

    Refactor student data processing and output

--- September-15-2026-MapsOfStructs/main.go
+++ September-15-2026-MapsOfStructs/main.go
@@ -13,12 +13,11 @@ func main() {
 	var studentData string
 	fmt.Scanln(&numStudentsStr)
 	fmt.Scanln(&studentData)
-    numStudents, err := strconv.Atoi(numStudentsStr)
-    if err != nil {
-        fmt.Printf("Invalid digit: %v\n", err)
-        return
-    }
-	
+	numbers, err := strconv.Atoi(numStudentsStr)
+	if err != nil {
+		fmt.Printf("Invalid number: %v", err)
+		return
+	}
 	// TODO: Определите структуру Student здесь
 	type Student struct {
 		ID int
@@ -27,47 +26,48 @@ func main() {
 	// TODO: Создайте карту (map) для хранения студентов (имя как ключ, структура Student как значение)
 	student := map[string]Student{}
 	// TODO: Разберите данные о студентах и заполните карту
-	divide := strings.Split(studentData, ",")
-	for count := 0; count < len(divide); count++ {
-		notes := strings.Split(divide[count], ":")
-		notesDigit, err := strconv.Atoi(notes[1])
-		if err != nil {
-			fmt.Printf("Invalid digit: %v", err)
-			return
-		}
-		noteStudent := Student{notesDigit, notes[2]}
-		student[notes[0]] = noteStudent
+	dividedStudentData := strings.Split(studentData, ",")
+	for i := 0; i < len(dividedStudentData); i++ {
+		doubleDividedStudentData := strings.Split(dividedStudentData[i], ":")
+        numberID, err := strconv.Atoi(doubleDividedStudentData[1])
+	    if err != nil {
+	    	fmt.Printf("Invalid ID: %v\n", err)
+	    	return
+	    }
+        students := Student{numberID, doubleDividedStudentData[2]}
+        student[doubleDividedStudentData[0]] = students
 	}
 	// TODO: Выведите всех студентов в алфавитном порядке по имени
-	names := make([]string, 0, len(student))
-	for n := range student {
-		names = append(names, n)
-	}
-	sort.Strings(names)
-	for n := range names {
-		fmt.Printf("%s: ID %d, Grade %s\n", names[n], student[names[n]].ID, student[names[n]].Grade)
-	}
+	sorted := []string{}
+    for key := range student {
+        sorted = append(sorted, key)
+    }
+    sort.Strings(sorted)
+    for print := 0; print < len(sorted); print++ {
+        fmt.Printf("%s: ID %d, Grade %s\n", sorted[print], student[sorted[print]].ID, student[sorted[print]].Grade)
+    }
 	// TODO: Рассчитайте и выведите статистику оценок
-	grades := map[string]int{}
-	for i := 0; i < len(names); i++ {
-		grades[student[names[i]].Grade]++
-	}
-    	gradeOrder := []string{"A", "B", "C", "D", "F"}
-    for i := 0; i < len(gradeOrder); i ++ {
-        if grades[gradeOrder[i]] > 0 {
-            fmt.Printf("Grade %s: %d students\n", gradeOrder[i], grades[gradeOrder[i]])
+    gradeScore := map[string]int{}
+	for gradeCount := 0; gradeCount < len(sorted); gradeCount++ {
+        gradeScore[student[sorted[gradeCount]].Grade]++ 
+    }
+    gradeAlpha := []string{"A", "B", "C", "D", "F"}
+    for count := 0; count < len(gradeAlpha); count++ {
+        if gradeScore[gradeAlpha[count]] > 0 {
+            fmt.Printf("Grade %s: %d students\n", gradeAlpha[count], gradeScore[gradeAlpha[count]])
         }
     }
 	// TODO: Найдите и выведите студента с самым высоким ID
-    maxID := student[names[0]].ID
-    maxIDname := names[0]
-    for count := 0; count < len(names); count++ {
-        if student[names[count]].ID > maxID {
-            maxID = student[names[count]].ID
-            maxIDname = names[count]
+    maxID := student[sorted[0]].ID
+    maxIDname := sorted[0]
+    for i := 0; i < len(sorted); i++ {
+        if student[sorted[i]].ID > maxID {
+            maxID = student[sorted[i]].ID
+            maxIDname = sorted[i]
         }
     }
+	// TODO: Выведите общее количество студентов
     fmt.Printf("Highest ID: %s (%d)\n", maxIDname, maxID)
 	// TODO: Выведите общее количество студентов
-    fmt.Printf("Total students: %d\n", numStudents)
+    fmt.Printf("Total students: %d\n", numbers)
 }
